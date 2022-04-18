3679770cafab5c10ab678698052b4deb54808848
Avery <avery@avery.cafe>
2022-04-17 23:52:06 -0400

    Add a loop to patch Steam as it starts and stops

--- main.go
+++ main.go
@@ -53,45 +53,61 @@ func run() error {
 		return err
 	}
 
-	waitForSteamProcess()
-
-	cdp.WaitForConnection(*debugPort)
-
-	err = cdp.WaitForLibraryEl(*debugPort)
-	if err != nil {
-		return err
-	}
-
 	// Patch and bundle in parallel
 	var wg sync.WaitGroup
-	wg.Add(2)
-
-	go func() {
-		defer wg.Done()
-		if *skipPatching {
-			return
-		}
-		patcher.Patch(*debugPort, *serverPort)
-	}()
+	wg.Add(1)
 
 	go func() {
 		defer wg.Done()
 		build.BundleScripts()
 	}()
 
+	// If Steam is already running we can patch it while bundling
+	alreadyPatched := false
+	if isSteamRunning() && !*skipPatching {
+		wg.Add(1)
+		alreadyPatched = true
+
+		go func() {
+			defer wg.Done()
+			cdp.WaitForConnection(*debugPort)
+			cdp.WaitForLibraryEl(*debugPort)
+			patcher.Patch(*debugPort, *serverPort)
+		}()
+	}
+
+	// Start RPC server in the background
+	// This will keep running in the background, so we don't need to add it to the wait group
+	go func() {
+		rpcServer := rpc.HandleRpc(*debugPort, *serverPort, plugins)
+
+		http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
+		http.Handle("/rpc", handlers.CORS(
+			handlers.AllowedHeaders([]string{"Content-Type"}),
+			handlers.AllowedMethods([]string{"POST"}),
+			handlers.AllowedOrigins([]string{"https://steamloopback.host"}),
+		)(rpcServer))
+
+		fmt.Println("Listening on :" + *serverPort)
+		log.Fatal(http.ListenAndServe(":"+*serverPort, nil))
+	}()
+
 	wg.Wait()
 
-	rpcServer := rpc.HandleRpc(*debugPort, *serverPort, plugins)
+	// If Steam was already running and we patched it earlier, wait for Steam to stop first
+	if alreadyPatched {
+		waitForSteamProcessToStop()
+	}
 
-	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
-	http.Handle("/rpc", handlers.CORS(
-		handlers.AllowedHeaders([]string{"Content-Type"}),
-		handlers.AllowedMethods([]string{"POST"}),
-		handlers.AllowedOrigins([]string{"https://steamloopback.host"}),
-	)(rpcServer))
+	// Repatch Steam as it starts and stops
+	for {
+		fmt.Println("Waiting for Steam to start...")
+		waitForSteamProcess()
 
-	fmt.Println("Listening on :" + *serverPort)
-	log.Fatal(http.ListenAndServe(":"+*serverPort, nil))
+		cdp.WaitForConnection(*debugPort)
+		cdp.WaitForLibraryEl(*debugPort)
+		patcher.Patch(*debugPort, *serverPort)
 
-	return nil
+		waitForSteamProcessToStop()
+	}
 }
--- ps.go
+++ ps.go
@@ -6,14 +6,30 @@ import (
 	"time"
 )
 
+// isSteamRunning checks if the steamwebhelper process is running.
+func isSteamRunning() bool {
+	cmd := exec.Command("bash", "-c", "ps -ef | grep steamwebhelper | grep -v grep")
+	return cmd.Run() == nil
+}
+
+// waitForSteamProcess waits for the steamwebhelper process to start.
 func waitForSteamProcess() {
 	fmt.Println("Waiting for steamwebhelper...")
 	for {
-		cmd := exec.Command("bash", "-c", "ps -ef | grep steamwebhelper | grep -v grep")
-		if cmd.Run() == nil {
+		if isSteamRunning() {
 			break
 		}
 		time.Sleep(1 * time.Second)
 	}
 	fmt.Println("steamwebhelper found!")
+}
+
+// waitForSteamProcessToStop waits for the steamwebhelper process to stop.
+func waitForSteamProcessToStop() {
+	for {
+		if !isSteamRunning() {
+			break
+		}
+		time.Sleep(1 * time.Second)
+	}
 }
