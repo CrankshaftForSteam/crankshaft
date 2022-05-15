25a9e0ddb62a50efa183d1e0831ca67b69834797
Avery <avery@avery.cafe>
2022-05-15 00:30:46 -0400

    Fix flag parsing

--- config/flags.go
+++ config/flags.go
@@ -19,13 +19,23 @@ func ParseFlags() (debugPort string, serverPort string, skipPatching bool, dataD
 	dataHome := GetXdgDataHome()
 	stateHome := GetXdgStateHome()
 
-	debugPort = *flag.String("debug-port", "8080", "CEF debug port")
-	serverPort = *flag.String("server-port", "8085", "Port to run HTTP/websocket server on")
-	skipPatching = *flag.Bool("skip-patching", false, "Skip patching Steam client resources")
-	dataDir = *flag.String("data-dir", path.Join(dataHome, "crankshaft"), "Crankshaft data directory")
-	pluginsDir = *flag.String("plugins-dir", path.Join(dataHome, "crankshaft", "plugins"), "Directory to load plugins from")
-	logsDir = *flag.String("logs-dir", path.Join(stateHome, "crankshaft", "logs"), "Directory to write logs to")
-	steamPath = *flag.String("steam-path", path.Join(dataHome, "Steam"), "Path to Steam files")
+	fDebugPort := flag.String("debug-port", "8080", "CEF debug port")
+	fServerPort := flag.String("server-port", "8085", "Port to run HTTP/websocket server on")
+	fSkipPatching := flag.Bool("skip-patching", false, "Skip patching Steam client resources")
+	fDataDir := flag.String("data-dir", path.Join(dataHome, "crankshaft"), "Crankshaft data directory")
+	fPluginsDir := flag.String("plugins-dir", path.Join(dataHome, "crankshaft", "plugins"), "Directory to load plugins from")
+	fLogsDir := flag.String("logs-dir", path.Join(stateHome, "crankshaft", "logs"), "Directory to write logs to")
+	fSteamPath := flag.String("steam-path", path.Join(dataHome, "Steam"), "Path to Steam files")
+
 	flag.Parse()
+
+	debugPort = *fDebugPort
+	serverPort = *fServerPort
+	skipPatching = *fSkipPatching
+	dataDir = *fDataDir
+	pluginsDir = *fPluginsDir
+	logsDir = *fLogsDir
+	steamPath = *fSteamPath
+
 	return
 }
