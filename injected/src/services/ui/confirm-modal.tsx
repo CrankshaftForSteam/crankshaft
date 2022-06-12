09f64c844f7f6b63fe52bc433a88988e7604354c
Avery <avery@avery.cafe>
2022-06-12 03:21:26 -0400

    Add confirm modal UI component

---
+++ injected/src/services/ui/confirm-modal.tsx
@@ -0,0 +1,98 @@
+// @use-dom-chef
+
+import { dcCreateElement } from '../../dom-chef';
+
+export const confirm = async ({
+  onConfirm,
+  onCancel,
+  ...props
+}: Parameters<typeof createConfirmModal>[0]) =>
+  new Promise<void>((resolve, reject) => {
+    const handleConfirm = () => {
+      onConfirm();
+      modal.remove();
+      resolve();
+    };
+
+    const handleCancel = () => {
+      onCancel();
+      modal.remove();
+      reject();
+    };
+
+    const modal = createConfirmModal({
+      ...props,
+      onConfirm: handleConfirm,
+      onCancel: handleCancel,
+    });
+
+    document.body.appendChild(modal);
+  });
+
+export const createConfirmModal = ({
+  message,
+  confirmText = 'Confirm',
+  cancelText = 'Cancel',
+  onConfirm,
+  onCancel,
+}: {
+  message: string;
+  confirmText: string;
+  cancelText: string;
+  onConfirm: () => void;
+  onCancel: () => void;
+}) => {
+  const uiMode = window.smmUIMode;
+
+  const modal = dcCreateElement<HTMLDivElement>(
+    <div
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        margin: 'auto',
+        zIndex: 9999,
+
+        height: 'min-content',
+        maxWidth: uiMode === 'deck' ? '40%' : '30%',
+
+        display: 'flex !important',
+        flexDirection: 'column',
+
+        backgroundColor: '#23262e',
+        borderRadius: 8,
+        boxShadow: '-12px 18px 24px -12px rgba(0, 0, 0, 0.5)',
+        color: '#b8bcbf',
+        padding: 20,
+      }}
+    >
+      <span>{message}</span>
+      <div
+        style={{
+          display: 'flex',
+          gap: 16,
+          marginTop: 16,
+        }}
+      >
+        <button
+          className="cs-button"
+          onClick={onConfirm}
+          style={{ width: '50%' }}
+        >
+          {confirmText}
+        </button>
+        <button
+          className="cs-button"
+          onClick={onCancel}
+          style={{ width: '50%', backgroundColor: 'var(--cs-col-secondary)' }}
+        >
+          {cancelText}
+        </button>
+      </div>
+    </div>
+  );
+
+  return modal;
+};
--- injected/src/services/ui/index.ts
+++ injected/src/services/ui/index.ts
@@ -1,8 +1,18 @@
+import { SMM } from '../../SMM';
 import { Service } from '../Service';
+import { confirm, createConfirmModal } from './confirm-modal';
 import { createProgressModal } from './progress-modal';
 
 export class UI extends Service {
-  createProgressModal(...args: Parameters<typeof createProgressModal>) {
-    return createProgressModal(...args);
+  confirm: typeof confirm;
+  createConfirmModal: typeof createConfirmModal;
+  createProgressModal: typeof createProgressModal;
+
+  constructor(smm: SMM) {
+    super(smm);
+
+    this.confirm = confirm;
+    this.createConfirmModal = createConfirmModal;
+    this.createProgressModal = createProgressModal;
   }
 }
