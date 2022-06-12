b833b2f6a0452729ec3543a08acaf55d817852d8
Avery <avery@avery.cafe>
2022-06-12 17:57:05 -0400

    Don't run commands with flatpak-spawn

--- executil/executil.go
+++ executil/executil.go
@@ -1,16 +1,15 @@
 package executil
 
-import "os/exec"
+import (
+	"log"
+	"os/exec"
+)
 
 var flatpak = false
 
 // Command wraps exec.Command to use flatpak-spawn when Crankshaft is running
 // inside the Flatpak sandbox.
 func Command(name string, args ...string) *exec.Cmd {
-	if flatpak {
-		cmdArgs := []string{"--host", name}
-		cmdArgs = append(cmdArgs, args...)
-		return exec.Command("flatpak-spawn", cmdArgs...)
-	}
+	log.Println("Command: ", name, args)
 	return exec.Command(name, args...)
 }
