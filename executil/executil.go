27a7050bfaf3d7bab26641af6cd3616d12596a7b
Avery <avery@avery.cafe>
2022-07-15 20:36:21 -0400

    (hopefully) temporary hack to get exec'd GUI apps working

--- executil/executil.go
+++ executil/executil.go
@@ -1,14 +1,29 @@
 package executil
 
-import "os/exec"
+import (
+	"os/exec"
+)
 
 var flatpak = false
+
+func getDisplay() string {
+	// TODO: this might be* a lie
+	return ":0"
+
+	// *definitely is
+}
 
 // Command wraps exec.Command to use flatpak-spawn when Crankshaft is running
 // inside the Flatpak sandbox.
 func Command(name string, args ...string) *exec.Cmd {
 	if flatpak {
-		cmdArgs := []string{"--host", name}
+		cmdArgs := []string{
+			// Run command on host
+			"--host",
+			// Passthrough display env variable for graphical apps
+			"--env=DISPLAY=" + getDisplay(),
+			name,
+		}
 		cmdArgs = append(cmdArgs, args...)
 		return exec.Command("flatpak-spawn", cmdArgs...)
 	}
