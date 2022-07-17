79c8bbe5c7f40149147d1edbd6fe282827a0aea2
Avery <avery@avery.cafe>
2022-07-17 14:19:03 -0400

    Cache plugins in plugins browser and add a refresh button

--- injected/src/internal-plugins/plugin-browser/index.tsx
+++ injected/src/internal-plugins/plugin-browser/index.tsx
@@ -20,6 +20,24 @@ export const load = (smm: SMM) => {
   });
 };
 
+let cachedPlugins: Plugin[] | undefined;
+const fetchPlugins = async (smm: SMM, refresh: boolean = false) => {
+  if (cachedPlugins && !refresh) {
+    return cachedPlugins;
+  }
+
+  const data = await smm.Network.get<Record<string, Omit<Plugin, 'id'>>>(
+    PLUGINS_URL
+  );
+
+  cachedPlugins = Object.entries(data).map(([id, plugin]) => ({
+    id,
+    ...plugin,
+  }));
+
+  return cachedPlugins;
+};
+
 export interface Plugin {
   id: string;
 
@@ -46,17 +64,13 @@ const PLUGINS_URL = 'https://crankshaft.space/plugins.json';
 
 const App: FunctionComponent<{ smm: SMM }> = ({ smm }) => {
   const [plugins, setPlugins] = useState<Plugin[] | undefined>(undefined);
-  const getPlugins = useCallback(async () => {
-    const data = await smm.Network.get<Record<string, Omit<Plugin, 'id'>>>(
-      PLUGINS_URL
-    );
-    setPlugins(
-      Object.entries(data).map(([id, plugin]) => ({
-        id,
-        ...plugin,
-      }))
-    );
-  }, [setPlugins, smm]);
+  const getPlugins = useCallback(
+    async (refresh: boolean = false) => {
+      setPlugins(undefined);
+      setPlugins(await fetchPlugins(smm, refresh));
+    },
+    [setPlugins, smm]
+  );
 
   const [installedPlugins, setInstalledPlugins] = useState<
     Record<string, InstalledPlugin> | undefined
@@ -80,9 +94,12 @@ const App: FunctionComponent<{ smm: SMM }> = ({ smm }) => {
 
   return (
     <>
-      <h1 style={{ fontSize: 24, margin: 'unset', marginBottom: 16 }}>
-        Get Plugins
-      </h1>
+      <div style={{ display: 'flex', marginBottom: 16, gap: 16 }}>
+        <h1 style={{ fontSize: 24, margin: 'unset' }}>Get Plugins</h1>
+        <button className="cs-button" onClick={() => getPlugins(true)}>
+          Refresh
+        </button>
+      </div>
       <ul
         style={{
           listStyle: 'none',
